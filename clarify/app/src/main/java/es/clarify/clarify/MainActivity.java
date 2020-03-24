560c1749e252b30cc66dfe7fa695da120f7b2ae1
Álvaro de la Flor Bonilla <alvarodelaflor@gmail.com>
2020-03-24 19:09:52 +0100

    Fragment

--- clarify/app/src/main/java/es/clarify/clarify/MainActivity.java
+++ clarify/app/src/main/java/es/clarify/clarify/MainActivity.java
@@ -21,6 +21,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -30,56 +31,69 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
-import java.net.URL;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import es.clarify.clarify.Login.Login;
-import es.clarify.clarify.NFC.NdefMessageParser;
+import es.clarify.clarify.Search.NfcIdentifyFragment;
 import es.clarify.clarify.NFC.NfcUtility;
-import es.clarify.clarify.NFC.ParsedNdefRecord;
-import es.clarify.clarify.Objects.FirebaseReferences;
 import es.clarify.clarify.Objects.ScannedTag;
-import es.clarify.clarify.Search.IdentifyFragment;
 import es.clarify.clarify.Utilities.UtilitiesFirebase;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView text;
-    private TextView text_company;
-    private TextView text_model;
-    private TextView text_expiration_date;
     private Button logout_button;
     private NfcAdapter nfcAdapter;
     private PendingIntent pendingIntent;
-    private BottomNavigationView bottomNavigationView;
-    private FrameLayout frameLayout;
-    private IdentifyFragment identifyFragment;
     private NfcUtility nfcUtility = new NfcUtility();
-    List<ScannedTag> scannedTagList;
+    private BottomNavigationView bottomNavigationView;
+    private FrameLayout mainFrame;
+    private NfcIdentifyFragment nfcIdentifyFragment;
+    private HomeFragment homeFragment;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        text = (TextView) findViewById(R.id.text);
-        text_company = (TextView) findViewById(R.id.text_company);
-        text_model = (TextView) findViewById(R.id.text_model);
-        text_expiration_date = (TextView) findViewById(R.id.text_expiration_date);
-        logout_button = (Button) findViewById(R.id.logOut);
-
-        identifyFragment = new IdentifyFragment();
+        // NFC instances
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
 
         // Toolbar instances
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        // NFC instances
-        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        // Set Fragment options
+        nfcIdentifyFragment = new NfcIdentifyFragment();
+        homeFragment = new HomeFragment();
+        mainFrame = (FrameLayout) findViewById(R.id.mainFrame);
+        setContentView(R.layout.activity_main); // Init main frame for the first boot
+
+        // Set navBar
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.nav_home:
+                        setFragment(homeFragment);
+                    case R.id.nav_folder:
+//                        TODO
+                        return true;
+                    case R.id.nav_explore:
+                        setFragment(nfcIdentifyFragment);
+                        return true;
+                }
+                return false;
+            }
+        });
+
+        pendingIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, this.getClass())
+                        .addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+
+
 
         // Firebase instances
 
@@ -90,83 +104,25 @@ public class MainActivity extends AppCompatActivity {
 //        databaseReference2.child("carlosjavier@gmail,com").child("stores").child("wardrobe").push().setValue(scannedTagPush);
         ///////////////////   CREATE INSTANCE    /////////////////////////////////////////////////////////////////////////
 
-        scannedTagList = new ArrayList<>();
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference databaseReference = database.getReference().child(("public"));
-        Query query = databaseReference.child("tags").orderByChild("id").equalTo("41520");
-        query.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                if (dataSnapshot.exists()) {
-                    // dataSnapshot is the "issue" node with all children with id 0
-                    for (DataSnapshot scannedTagFirebase : dataSnapshot.getChildren()) {
-                        Log.d("FIREBASE_ASYN_1", scannedTagFirebase.toString());
-                        ScannedTag scannedTag = scannedTagFirebase.getValue(ScannedTag.class);
-                        Log.d("FIREBASE_ASYN_2", scannedTag.toString());
-                        scannedTagList.add(scannedTag);
-                    }
-                }
-            }
+//        logout_button = (Button) findViewById(R.id.logOut)
+//        logout_button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                logout(v);
+//                startActivity(new Intent(getApplicationContext(),Login.class));
+//            }
+//        });
 
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-
-        if (nfcAdapter == null) {
-            Toast.makeText(this, "Dispositivo incompatible", Toast.LENGTH_LONG).show();
-            finish();
-            return;
-        }
-
-
-        pendingIntent = PendingIntent.getActivity(this, 0,
-                new Intent(this, this.getClass())
-                        .addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
-
-        logout_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                logout(v);
-                startActivity(new Intent(getApplicationContext(),Login.class));
-            }
-        });
-
-        /*
-        setFragment(identifyFragment);
-
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch (menuItem.getItemId()) {
-                    case R.id.nav_search:
-                        setFragment(identifyFragment);
-                        bottomNavigationView.setItemBackgroundResource(R.color.gray);
-                        return true;
-                    case R.id.nav_folder:
-                        bottomNavigationView.setItemBackgroundResource(R.color.colorPrimary);
-                        return true;
-                    case R.id.nav_explore:
-                        bottomNavigationView.setItemBackgroundResource(R.color.colorAccent);
-                        return true;
-                }
-                return false;
-            }
-        });
-
-         */
 
     }
-    /*
+
     private void setFragment(Fragment fragment) {
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.identifyFragment, fragment);
+        fragmentTransaction.replace(R.id.mainFrame, fragment);
         fragmentTransaction.commit();
 
     }
-     */
 
     public void logout(View view) {
         FirebaseAuth.getInstance().signOut();//logout
@@ -195,43 +151,43 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onNewIntent(Intent intent) {
+        setIntent(intent);
+        nfcIdentifyFragment.resolveIntent(intent);
+    }
+
+    @Override
     protected void onPause() {
         super.onPause();
     }
 
-    @Override
-    protected void onNewIntent(Intent intent) {
-        setIntent(intent);
-        resolveIntent(intent);
-    }
-
-    private void resolveIntent(Intent intent) {
-        String action = intent.getAction();
-
-        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)
-                || NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)
-                || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
-            Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
-            NdefMessage[] msgs;
-
-            if (rawMsgs != null) {
-                msgs = new NdefMessage[rawMsgs.length];
-
-                for (int i = 0; i < rawMsgs.length; i++) {
-                    msgs[i] = (NdefMessage) rawMsgs[i];
-                }
-
-            } else {
-                byte[] empty = new byte[0];
-                byte[] id = intent.getByteArrayExtra(NfcAdapter.EXTRA_ID);
-                Tag tag = (Tag) intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-                byte[] payload = nfcUtility.dumpTagData(tag).getBytes();
-                NdefRecord record = new NdefRecord(NdefRecord.TNF_UNKNOWN, empty, id, payload);
-                NdefMessage msg = new NdefMessage(new NdefRecord[] {record});
-                msgs = new NdefMessage[] {msg};
-            }
-            UtilitiesFirebase utilitiesFirebase = new UtilitiesFirebase(msgs, Arrays.asList(text, text_company, text_model, text_expiration_date));
-            utilitiesFirebase.printInfo();
-        }
-    }
+//    private void resolveIntent(Intent intent) {
+//        String action = intent.getAction();
+//
+//        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)
+//                || NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)
+//                || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+//            Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+//            NdefMessage[] msgs;
+//
+//            if (rawMsgs != null) {
+//                msgs = new NdefMessage[rawMsgs.length];
+//
+//                for (int i = 0; i < rawMsgs.length; i++) {
+//                    msgs[i] = (NdefMessage) rawMsgs[i];
+//                }
+//
+//            } else {
+//                byte[] empty = new byte[0];
+//                byte[] id = intent.getByteArrayExtra(NfcAdapter.EXTRA_ID);
+//                Tag tag = (Tag) intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+//                byte[] payload = nfcUtility.dumpTagData(tag).getBytes();
+//                NdefRecord record = new NdefRecord(NdefRecord.TNF_UNKNOWN, empty, id, payload);
+//                NdefMessage msg = new NdefMessage(new NdefRecord[]{record});
+//                msgs = new NdefMessage[]{msg};
+//            }
+//            UtilitiesFirebase utilitiesFirebase = new UtilitiesFirebase(msgs, Arrays.asList(text, text_company, text_model, text_expiration_date));
+//            utilitiesFirebase.printInfo();
+//        }
+//    }
 }
---
+++ clarify/app/src/main/java/es/clarify/clarify/Search/NfcIdentifyFragment.java
@@ -0,0 +1,100 @@
+package es.clarify.clarify.Search;
+
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.os.Parcelable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Arrays;
+
+import es.clarify.clarify.NFC.NfcUtility;
+import es.clarify.clarify.R;
+import es.clarify.clarify.Utilities.UtilitiesFirebase;
+
+
+public class NfcIdentifyFragment extends Fragment {
+
+    private NfcAdapter nfcAdapter;
+    private NfcUtility nfcUtility = new NfcUtility();
+
+    private TextView text;
+    private TextView text_company;
+    private TextView text_model;
+    private TextView text_expiration_date;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_nfc_identify, container, false);
+        text = (TextView) v.findViewById(R.id.product);
+        text_company = (TextView) v.findViewById(R.id.text_company2);
+        text_model = (TextView) v.findViewById(R.id.text_model2);
+        text_expiration_date = (TextView) v.findViewById(R.id.text_expiration_date2);
+
+
+        return v;
+    }
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // NFC instances
+        nfcAdapter = NfcAdapter.getDefaultAdapter(getContext());
+
+        if (nfcAdapter == null) {
+            Toast.makeText(getContext(), "Dispositivo incompatible", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+    }
+
+    public void resolveIntent(Intent intent) {
+        String action = intent.getAction();
+
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)
+                || NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)
+                || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+            Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            NdefMessage[] msgs;
+
+            if (rawMsgs != null) {
+                msgs = new NdefMessage[rawMsgs.length];
+
+                for (int i = 0; i < rawMsgs.length; i++) {
+                    msgs[i] = (NdefMessage) rawMsgs[i];
+                }
+
+            } else {
+                byte[] empty = new byte[0];
+                byte[] id = intent.getByteArrayExtra(NfcAdapter.EXTRA_ID);
+                Tag tag = (Tag) intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+                byte[] payload = nfcUtility.dumpTagData(tag).getBytes();
+                NdefRecord record = new NdefRecord(NdefRecord.TNF_UNKNOWN, empty, id, payload);
+                NdefMessage msg = new NdefMessage(new NdefRecord[]{record});
+                msgs = new NdefMessage[]{msg};
+            }
+            UtilitiesFirebase utilitiesFirebase = new UtilitiesFirebase(msgs, Arrays.asList(text, text_company, text_model, text_expiration_date));
+            utilitiesFirebase.printInfo();
+        }
+    }
+
+}
