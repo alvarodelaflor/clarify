e04c884ea27852c451c1dd757e9cb9b894a9bb19
Álvaro de la Flor Bonilla <alvarodelaflor@gmail.com>
2020-04-14 14:56:23 +0200

    [feat] New list of boxes

--- clarify/app/src/main/java/es/clarify/clarify/StoreFragment.java
+++ clarify/app/src/main/java/es/clarify/clarify/StoreFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -47,7 +48,7 @@ public class StoreFragment extends Fragment {
         textViewPrincipal = (TextView) v.findViewById(R.id.text_second);
         textViewPrincipal.setText(listStoreLocal.size() + " boxes");
         RecyclerViewAdapter recyclerViewAdapter = new RecyclerViewAdapter(getContext(), listStoreLocal);
-        myRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        myRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
         myRecyclerView.setAdapter(recyclerViewAdapter);
         return v;
     }
