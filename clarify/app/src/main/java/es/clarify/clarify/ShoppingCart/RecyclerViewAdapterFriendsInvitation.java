a96e59d857bb4f7b1ed00a927f5f62d397b88b12
Alvaro de la Flor Bonilla <alvarodelaflor@gmail.com>
2020-05-02 15:04:15 +0200

    [feat] Show dialog  invitation list

--- clarify/app/src/main/java/es/clarify/clarify/ShoppingCart/RecyclerViewAdapterFriendsInvitation.java
+++ clarify/app/src/main/java/es/clarify/clarify/ShoppingCart/RecyclerViewAdapterFriendsInvitation.java
@@ -1,6 +1,9 @@
 package es.clarify.clarify.ShoppingCart;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,10 +16,12 @@ import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
+import de.hdodenhof.circleimageview.CircleImageView;
 import es.clarify.clarify.Objects.FriendLocal;
 import es.clarify.clarify.Objects.PurchaseLocal;
 import es.clarify.clarify.R;
@@ -45,6 +50,11 @@ public class RecyclerViewAdapterFriendsInvitation extends RecyclerView.Adapter<R
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        Dialog dialog = new Dialog(mContext);
+        dialog.setContentView(R.layout.dialog_show_invitation);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
         holder.name.setText(mDataFriendsInvitation.get(holder.getAdapterPosition()).getName());
         holder.email.setText(mDataFriendsInvitation.get(holder.getAdapterPosition()).getEmail());
         Picasso.get().load(mDataFriendsInvitation.get(holder.getAdapterPosition()).getPhoto()).into(holder.img);
@@ -53,6 +63,19 @@ public class RecyclerViewAdapterFriendsInvitation extends RecyclerView.Adapter<R
         } else {
             holder.pending.setVisibility(View.VISIBLE);
         }
+        CircleImageView imgDialog = (CircleImageView) dialog.findViewById(R.id.image_friend_dialog);
+        TextView nameDialog = (TextView) dialog.findViewById(R.id.name_user_dialog);
+        TextView emailDialog = (TextView) dialog.findViewById(R.id.email_user_dialog);
+        nameDialog.setText(mDataFriendsInvitation.get(holder.getAdapterPosition()).getName());
+        emailDialog.setText(mDataFriendsInvitation.get(holder.getAdapterPosition()).getEmail());
+        Glide.with(mContext).load(mDataFriendsInvitation.get(holder.getAdapterPosition()).getPhoto()).into(imgDialog);
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.show();
+            }
+        });
+
     }
 
     @Override
@@ -66,6 +89,7 @@ public class RecyclerViewAdapterFriendsInvitation extends RecyclerView.Adapter<R
         private TextView email;
         private ImageView img;
         private TextView pending;
+        private CardView cardView;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -74,6 +98,7 @@ public class RecyclerViewAdapterFriendsInvitation extends RecyclerView.Adapter<R
             email = (TextView) itemView.findViewById(R.id.email_friend_invitation);
             img = (ImageView) itemView.findViewById(R.id.img_friend);
             pending = (TextView) itemView.findViewById(R.id.pending);
+            cardView = (CardView) itemView.findViewById(R.id.card_view_stores);
         }
     }
 }
